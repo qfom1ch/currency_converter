b1a69e550a5b4cc11139ede372e0fb1fef878459
Sergey Fomichev <fomichev.ser.v@gmail.com>
2024-06-26 20:46:43 +0300

    fix depends

--- apps/currency/dependencies.py
+++ apps/currency/dependencies.py
@@ -2,5 +2,5 @@ from apps.currency.providers.currate.currate import CurrateCurrencyProvider
 from apps.currency.service.service import CurrencyService
 
 
-def get_currency_service_currate():
+async def get_currency_service_currate():
     return CurrencyService(CurrateCurrencyProvider())
